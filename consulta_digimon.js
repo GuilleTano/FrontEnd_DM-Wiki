a7e2627b5a75d97040aa7336873172070179aab3
GuilleTano <guillermotano@hotmail.com>
2022-09-08 19:11:13 -0300

    funciones varias

--- consulta_digimon.js
+++ consulta_digimon.js
@@ -4,8 +4,10 @@ function mostrarDigimon() {
 
     let addDigimon = `
 
-        <div>${digimonData.id}</div><br>
+        <div>${digimonData.id}</div>
+        <hr>
         <img src=${digimonData.images[0].href}>
+        <hr>
         <div>${digimonData.name}</div><br>
         <div>${digimonData.attributes[0].attribute}</div><br>
         <div>${digimonData.levels[0].level}</div><br>
@@ -15,20 +17,24 @@ function mostrarDigimon() {
 
     document.getElementById("mostrar-digimon").innerHTML = addDigimon;
 
+    document.getElementById("info-digimon").innerHTML = `
+    <p>${digimonData.descriptions[0].description}</p>
+    `;
+
 }
-/*
-document.addEventListener("DOMContentLoaded", function(){
-    getJSONData(DIGIMON_URL).then(resultObj => {
-        if (resultObj.status === "ok") {
-            digimonData = resultObj.data;
-            console.log(digimonData);
-            mostrarDigimon(digimonData);
-        }
-        else {
-            alert("Digi-error");
-        }
-    });
-*/
+
+function digimonSkills(){
+    let digimon_skills = [];
+
+    for (let i=0; i < digimonData.skills.length; i++){
+        
+        digimon_skills += `
+        <p>${digimonData.skills[i].skill}</p>
+        `;
+    }
+    console.log(digimon_skills);
+    document.getElementById("skill-digimon").innerHTML = digimon_skills;
+}
 
 document.getElementById("btnBuscar").addEventListener("click", function () {
 
@@ -40,6 +46,7 @@ document.getElementById("btnBuscar").addEventListener("click", function () {
             digimonData = resultObj.data;
             console.log(digimonData);
             mostrarDigimon(digimonData);
+            digimonSkills(digimonData);
         }
         else {
             alert("Digi-error");
