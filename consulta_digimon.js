e617a225ebcfb0dc0f93750bd7f001a2a980b7aa
GuilleTano <110430498+GuilleTano@users.noreply.github.com>
2022-09-09 17:23:10 -0300

    Seleccion de idioma en la descripcion Modificaciones en las tablas de la carta

--- consulta_digimon.js
+++ consulta_digimon.js
@@ -1,59 +1,124 @@
 let buscador = "";
 
-function mostrarLevels(){
+//ESTAS FUNCIONES MUESTRAN LOS DATOS EN LA CARTA
+function mostrarLevels() {
     let arrayLevels = [];
-    for (let i=0; i < digimonData.levels.length; i++){
-        
-        arrayLevels += `<p>${digimonData.levels[i].level}</p>`
-        
+    for (let i = 0; i < digimonData.levels.length; i++) {
+        arrayLevels += `<div>${digimonData.levels[i].level}</div>`;
     }
     return arrayLevels;
 }
 
+function mostrarAtributos() {
+    let arrayAtributos = [];
+    for (let i = 0; i < digimonData.attributes.length; i++) {
+        arrayAtributos += `<div>${digimonData.attributes[i].attribute}</div>`;
+    }
+    return arrayAtributos;
+}
+
+function mostrarTipos() {
+    let arrayTipos = [];
+    for (let i = 0; i < digimonData.types.length; i++) {
+        arrayTipos += `<div>${digimonData.types[i].type}</div>`;
+    }
+    return arrayTipos;
+}
+
+function mostrarFields() {
+    let arrayFields = [];
+    for (let i = 0; i < digimonData.fields.length; i++) {
+        arrayFields += `<td>${digimonData.fields[i].field}</td>`;
+    }
+    return arrayFields;
+}
+
+//FILTRA LAS DESCRIPCIONES PARA MOSTRAR LA QUE ESTA EN INGLES
+function mostrarDescripcion() {
+    let descripcion = [];
+    for (let i = 0; i < digimonData.descriptions.length; i++) {
+        if (digimonData.descriptions[i].language == "en_us") {
+            descripcion += `<p>${digimonData.descriptions[i].description}</p>`;
+        }
+    }
+    return descripcion;
+}
+
+function mostrarHabilidades() {
+    let habilidades = [];
+    for (let i = 0; i < digimonData.skills.length; i++) {
+        if (digimonData.skills[i].translation == "") {
+            habilidades += `
+            <p><strong>${digimonData.skills[i].skill}</strong></p>
+            <p>${digimonData.skills[i].description}</p>
+            <hr>
+            `;
+        }
+        else{
+            habilidades += `
+            <p><strong>${digimonData.skills[i].skill} / ${digimonData.skills[i].translation}</strong></p>
+            <p>${digimonData.skills[i].description}</p>
+            <hr>
+            `;
+        }
+    }
+    document.getElementById("skill-digimon").innerHTML = habilidades;
+
+}
+
 // FUNCION MOSTRAR - MUESTRA LA CARTA DEL DIGIMON Y LA DESCRIPCION
 function mostrarDigimon() {
-
     let addDigimon = `
-
         <div><p style="margin:5px">${digimonData.id}<br><strong>${digimonData.name}</strong></p></div>
         <hr style="margin:2px">
         <img src=${digimonData.images[0].href}>
         <hr>
         <table>
-        <thead>
-            <tr>
-                <th>Level</th>
-                <th>Attribute</th>
-                <th>Type</th>
-            </tr>
-        </thead>
-        <tbody>
-        <tr>
-            <td>${mostrarLevels()}</td>
-            <td> ${digimonData.attributes[0].attribute}</td>
-            <td> ${digimonData.types[0].type}</td>
-        </tr>
-        </tbody>
-    </table>
+            <thead>
+                <tr>
+                    <th>Level</th>
+                    <th>Attribute</th>
+                    <th>Type</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>${mostrarLevels()}</td>
+                    <td>${mostrarAtributos()}</td>
+                    <td>${mostrarTipos()}</td>
+                </tr>
+            </tbody>
+        </table>
         <hr>
-        <div> ${digimonData.xAntibody}</div>
+        <table>
+            <thead>
+                <tr>
+                    <th></th>
+                    <th>Fields</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    ${mostrarFields()}
+                </tr>
+            </tbody>
+        </table>
+        <hr>
+        <div><p>xAntibody: ${digimonData.xAntibody}</p></div>
         `;
-    
     document.getElementById("mostrar-digimon").innerHTML = addDigimon;
 
     //MUESTRA LA DESCRIPCION DEL DIGIMON 
-    document.getElementById("info-digimon").innerHTML = `
-    <p>${digimonData.descriptions[0].description}</p>
-    `;
-
+    document.getElementById("info-digimon").innerHTML = `<div>${mostrarDescripcion()}</div>`;
 }
 
 //FUNCION PARA MOSTRAR LAS SKILLS
-function digimonSkills(){
+/*
+function digimonSkills() {
     let digimon_skills = [];
+    for (let i = 0; i < digimonData.skills.length; i++) {
 
-    for (let i=0; i < digimonData.skills.length; i++){
-        
         digimon_skills += `
         <p><strong>${digimonData.skills[i].skill}/ ${digimonData.skills[i].translation}</strong></p>
         <p>${digimonData.skills[i].description}</p>
@@ -62,6 +127,7 @@ function digimonSkills(){
     }
     document.getElementById("skill-digimon").innerHTML = digimon_skills;
 }
+*/
 
 document.getElementById("btnBuscar").addEventListener("click", function () {
 
@@ -72,8 +138,11 @@ document.getElementById("btnBuscar").addEventListener("click", function () {
             digimonData = resultObj.data;
             console.log(digimonData);
             mostrarDigimon(digimonData);
-            digimonSkills(digimonData);
             mostrarLevels(digimonData);
+            mostrarAtributos(digimonData);
+            mostrarTipos(digimonData);
+            mostrarDescripcion(digimonData);
+            mostrarHabilidades(digimonData);
         }
         else {
             alert("Digi-error");
